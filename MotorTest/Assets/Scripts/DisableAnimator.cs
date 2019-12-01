d3a3b1ff7a32015e267574693ee005b27bd70433
Toms <toms.amsons@va.lv>
2019-12-01 20:31:33 +0200

    physics enable after animation

--- MotorTest/Assets/Scripts/DisableAnimator.cs
+++ MotorTest/Assets/Scripts/DisableAnimator.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class DisableAnimator : MonoBehaviour
 {
     private Animator m_Animator;
-
+    public List<GameObject> gameObjList;
+    public GameObject _armature;
     // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
+        GetBones();
     }
 
     // Update is called once per frame
@@ -18,6 +20,29 @@ public class DisableAnimator : MonoBehaviour
         if(m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !m_Animator.IsInTransition(0))
         {
             m_Animator.enabled = false;
+            EnablePhysics();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log(gameObjList.ToString());
+        }
+    }
+    void GetBones()
+    {
+        foreach(Transform child in _armature.transform)
+        {
+            if (child.tag == "Grab")
+            {
+                Debug.Log("add");
+                gameObjList.Add(child.gameObject);               
+            }
+        }
+    }
+    void EnablePhysics()
+    {
+        foreach(GameObject obj in gameObjList)
+        {
+            obj.GetComponent<Rigidbody>().isKinematic = false;
         }
     }
 }
