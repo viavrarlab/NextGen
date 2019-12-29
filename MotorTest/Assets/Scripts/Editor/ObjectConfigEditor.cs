f2bf4a1782adc7f04ab51a3fd039deba43f583b2
Toms <toms.amsons@va.lv>
2019-12-29 21:02:11 +0200

    Editor script begining

---
+++ MotorTest/Assets/Scripts/Editor/ObjectConfigEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ObjectConfig))]
+public class ObjectConfigEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        DrawDefaultInspector();
+
+        ObjectConfig Configs = (ObjectConfig)target;
+        if (GUILayout.Button("Configur"))
+        {
+            Configs.AddComponents();
+        }
+    }
+}
---
+++ MotorTest/Assets/Scripts/ObjectConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectConfig : MonoBehaviour
+{
+    public GameObject obj;
+    GameObject armature;
+    public List<GameObject> Bones;
+    public void FindArmature()
+    {
+        foreach(Transform child in obj.transform)
+        {
+            if(child.name == "Armature")
+            { 
+                armature = child.gameObject;
+                Debug.Log("Armatura atrasta");
+            }
+        }
+        foreach (Transform child in armature.transform)
+        {
+                Bones.Add(child.gameObject);
+                print("Ir kauli");
+        }
+
+    }
+    public void AddComponents()
+    {
+        FindArmature();
+        foreach(GameObject obj in Bones)
+        {
+            obj.AddComponent<Rigidbody>();
+            obj.AddComponent<MeshCollider>();
+        }
+
+    }
+}
