cdc54da83c060fdcdc139d49745bd3a20a3fb033
Toms <toms.amsons@va.lv>
2019-12-30 01:20:41 +0200

    Editor script add component and tag functionality

--- MotorTest/Assets/Scripts/Editor/ObjectConfigEditor.cs
+++ MotorTest/Assets/Scripts/Editor/ObjectConfigEditor.cs
@@ -6,16 +6,55 @@ using UnityEditor;
 [CustomEditor(typeof(ObjectConfig))]
 public class ObjectConfigEditor : Editor
 {
-
+    GameObject armature;
+    List<GameObject> Bones;
+    ObjectConfig Configs;
+    private void OnEnable()
+    {
+        Configs = (ObjectConfig)target;
+        Bones = new List<GameObject>();
+    }
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
         DrawDefaultInspector();
-
-        ObjectConfig Configs = (ObjectConfig)target;
+        
         if (GUILayout.Button("Configur"))
         {
-            Configs.AddComponents();
+            AddComponents();
+            AddTag();
+        }
+    }
+    public void FindArmature()
+    {
+        foreach (Transform child in Configs.gameObject.transform)
+        {
+            if (child.name == "Armature")
+            {
+                armature = child.gameObject;
+                Debug.Log("Armatura atrasta");
+            }
+        }
+        foreach (Transform child in armature.transform)
+        {
+            Bones.Add(child.gameObject);
+        }
+    }
+    public void AddComponents()
+    {
+        FindArmature();
+        foreach (GameObject obj in Bones)
+        {
+            obj.AddComponent<Rigidbody>();
+            obj.AddComponent<MeshCollider>();
+            Debug.Log("RigidBody and MeshCollider added");
+        }
+    }
+    public void AddTag()
+    {
+        foreach(GameObject obj in Bones)
+        {
+            armature.tag = "Armature";
+            obj.tag = "Grab";
         }
     }
 }
--- MotorTest/Assets/Scripts/ObjectConfig.cs
+++ MotorTest/Assets/Scripts/ObjectConfig.cs
@@ -4,34 +4,5 @@ using UnityEngine;
 
 public class ObjectConfig : MonoBehaviour
 {
-    public GameObject obj;
-    GameObject armature;
-    public List<GameObject> Bones;
-    public void FindArmature()
-    {
-        foreach(Transform child in obj.transform)
-        {
-            if(child.name == "Armature")
-            { 
-                armature = child.gameObject;
-                Debug.Log("Armatura atrasta");
-            }
-        }
-        foreach (Transform child in armature.transform)
-        {
-                Bones.Add(child.gameObject);
-                print("Ir kauli");
-        }
-
-    }
-    public void AddComponents()
-    {
-        FindArmature();
-        foreach(GameObject obj in Bones)
-        {
-            obj.AddComponent<Rigidbody>();
-            obj.AddComponent<MeshCollider>();
-        }
-
-    }
+    
 }
