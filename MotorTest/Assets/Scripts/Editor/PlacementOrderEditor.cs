217cf3e512cee02b2027af248da3cd70f9f51546
Toms <toms.amsons@va.lv>
2020-03-17 21:50:04 +0200

    Motor parts naming/pictures and exploded view

---
+++ MotorTest/Assets/Scripts/Editor/PlacementOrderEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(CorrectOrderTests))]
+public class PlacementOrderEditor : Editor
+{
+    CorrectOrderTests m_CorrOrder;
+    private void OnEnable()
+    {
+        m_CorrOrder = (CorrectOrderTests)target;
+    }
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+    }
+}
