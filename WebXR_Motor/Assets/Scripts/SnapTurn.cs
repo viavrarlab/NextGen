680b0d051a938da1444f637235cf47892245370b
DaviisLV <abols.davis@gmail.com>
2020-10-22 15:19:40 +0300

    sceen update

--- WebXR_Motor/Assets/Scripts/SnapTurn.cs
+++ WebXR_Motor/Assets/Scripts/SnapTurn.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using HTC.UnityPlugin.Vive;
+//using HTC.UnityPlugin.Vive;
 
 public class SnapTurn : MonoBehaviour
 {
@@ -9,8 +9,8 @@ public class SnapTurn : MonoBehaviour
     public Transform m_VRCamera;
     public int m_TurnAngle = 45;
 
-    public HandRole m_HandRole;
-    public ControllerAxis m_ControllerAxis;
+    // public HandRole m_HandRole;
+    //public ControllerAxis m_ControllerAxis;
     public float m_AxisThreshold = 0.8f;
 
     private bool m_IsRotating = false;
@@ -24,27 +24,35 @@ public class SnapTurn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float value = ViveInput.GetAxisEx(m_HandRole, m_ControllerAxis);
+        // float value = ViveInput.GetAxisEx(m_HandRole, m_ControllerAxis);
 
-        if ((value > m_AxisThreshold || value < -m_AxisThreshold)/* && !m_IsRotating*/)
-        {
-            if (m_IsRotating == false)
-            {
-                Rotate(Mathf.Sign(value));
-            }
-        }
-        else
-        {
-            if (m_IsRotating == true)
-            {
-                m_IsRotating = false;
-            }
-        }
+        // if ((value > m_AxisThreshold || value < -m_AxisThreshold)/* && !m_IsRotating*/)
+        // {
+        //     if (m_IsRotating == false)
+        //     {
+        //         Rotate(Mathf.Sign(value));
+        //     }
+        // }
+        // else
+        // {
+        //     if (m_IsRotating == true)
+        //     {
+        //         m_IsRotating = false;
+        //     }
+        // }
+    }
+    public void RotateLeft()
+    {
+        Rotate(-1);
+    }
+      public void RotateRight()
+    {
+        Rotate(1);
     }
 
-    public void Rotate(float direction)
+    private void Rotate(float direction)
     {
-        m_IsRotating = true;
+        //   m_IsRotating = true;
         //Quaternion rotationAmount = Quaternion.Euler(0f,(m_TurnAngle * direction), 0f);
         //Quaternion postRotation = m_VRRig.rotation * rotationAmount;
         //m_VRRig.rotation = postRotation;
